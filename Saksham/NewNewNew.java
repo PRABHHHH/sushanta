511d61de9f344ce8903299829a0f0c987befb3d0
Prabhjot <Prabhjot.singh@moolya.com>
2022-04-19 23:15:15 +0530

    newcommit

---
+++ Saksham/NewNewNew.java
@@ -0,0 +1,10 @@
+package Saksham;
+
+public class NewNewNew {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Sushantha bhai jindabaad");
+	}
+
+}
